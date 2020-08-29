b6e4b97f8e850ecc7913b2a09fe350b7bbece81e
JulianDavidG07 <julygsaenz1995@hotmail.com>
2020-08-29 11:00:56 -0500

    [ADD] state aplication vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,23 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    heroesArray: [],
   },
   mutations: {
+    pushHeroesArray(state, heroesAction) {
+      state.heroesArray = heroesAction;
+    },
   },
   actions: {
+    getHeroesArray: async ({ commit }) => {
+      const data = await fetch("https://jsonplaceholder.typicode.com/posts");
+      const heroesArray = data.json();
+      commit("pushHeroesArray", heroesArray);
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
